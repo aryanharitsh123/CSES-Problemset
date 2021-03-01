f8860ccef4d9f87bcad413ffdbd1bfae41690d84
Aryan Sharma <aryanharitsh@Aryans-MacBook-Air.local>
2021-03-01 21:19:01 +0530

    1-03-2021

---
+++ Sorting And Searching/Sum_Of_Two_Values.cpp
@@ -0,0 +1,145 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+typedef long long int lli;
+ 
+#define all(arr) arr.begin(),arr.end()
+#define f first
+#define s second
+#define debug1(x) cout<<x<<"\n"
+#define debug2(x,y) cout<<x<<" "<<y<<"\n"
+#define debug3(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n"
+#define nl cout<<"\n";
+#define pq priority_queue
+#define inf 0x3f3f3f3f
+#define test cout<<"abcd\n";
+#define pi pair<int,int>
+#define pii pair<int,pi>
+#define pb push_back
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define MOD 1000000007
+ 
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+ 
+ 
+template <typename T>
+void input(vector<T> &arr,lli n) {
+  T temp;
+  for(lli i=0;i<n;i++) cin>>temp, arr.push_back(temp);
+}
+ 
+template <typename T>
+void output(vector<T> arr) {
+  for(auto x:arr) cout<<x<<" ";
+  cout<<endl;
+}
+ 
+ 
+template <typename T>
+void input_set(set<T> &arr,lli n) {
+  T temp;
+  for(lli i=0;i<n;i++) cin>>temp, arr.insert(temp);
+}
+ 
+lli mul(lli a, lli b) {
+  return (a%MOD*b%MOD)%MOD;
+}
+ 
+lli power(lli a,lli b) {
+  lli ans = 1;
+  while(b > 0) {
+    if(b&1)
+      ans = mul(ans, a);
+    a = mul(a,a);;
+    b >>= 1;
+  }
+  return ans;
+}
+void getIndex(vector<lli> v, int K)
+{
+    auto it = find(v.begin(), v.end(), K);
+    if (it != v.end()) 
+    {
+        int index = it - v.begin();
+        cout << index << endl;
+    }
+    else {
+        cout << "-1" << endl;
+    }
+}
+ 
+ 
+void solve() {
+  lli n,x;
+  cin >> n >> x;
+  vector<lli> vec;
+  input(vec,n);
+  vector<pair<lli,lli>>ind;
+ 
+  for(int a=0;a<n;a++){
+      ind.push_back(make_pair(vec[a],a));
+  }
+ 
+  sort(vec.begin(),vec.end()); 
+  bool flag=false;
+  int i=0,j=n-1;
+  lli no1,no2;
+  while(i<j){
+      if(vec[i]+vec[j]==x){
+          flag=true;
+          no1=vec[i];
+          no2=vec[j];
+          break;
+      }
+      else if(vec[i]+vec[j]<x){
+          i++;
+      }
+      else{
+          j--;
+      }
+  }
+ 
+  if(flag){
+      for(int a=0;a<n;a++){
+          if(ind[a].first==no1||ind[a].first==no2){
+              cout << ind[a].second + 1 << " ";
+          }
+      }
+  }
+  else{
+      cout << "IMPOSSIBLE";
+  }
+ 
+}
+ 
+int main() {
+  
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+    solve();
+}
+ 
